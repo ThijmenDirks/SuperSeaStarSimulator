86c11c4f4aca4862add3e6b96b7bef299e473604
ThijmenDirks <dirkssthijmen@gmail.com>
2025-08-06 15:13:31 +0200

    spawn_area Alpha
    - added thevery begining of spawnarea, although it will very likely be delted to make place for a better method
    
    - cleaned disintegrate a bit

---
+++ scripts/map/wave_logics.gd
@@ -0,0 +1,16 @@
+extends Node
+
+#dit worden slcecht de ew ave slogics
+
+#de wave st3eups worden in elk lvl gemaakt
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
