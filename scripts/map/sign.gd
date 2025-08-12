ad708542bdb4daac6b08e41ab9e7c12361f589d6
ThijmenDirks <dirkssthijmen@gmail.com>
2025-08-12 10:09:48 +0200

    sign V1
    -signs almost finished. only missing feature is that texts apear on one line, which makes long texts unreadable

--- scripts/map/sign.gd
+++ scripts/map/sign.gd
@@ -8,26 +8,29 @@ var player_is_near = false
 
 
 func _ready():
+	label.text = text
 	label.visible = false
 
 
-func _on_Area2D_body_entered(body):
+func _unhandled_input(event):
+	#print("sign, T has been pressed")
+	if event.is_action_pressed("T"):# and player_is_near:
+		print("sign, T has been pressed")
+		if player_is_near:
+			print("sign, show text")
+			label.visible = !label.visible
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("sign, body is near")
 	if body is Player:
 		player_is_near = true
 		print("sign, player is near")
 
 
-func _on_Area2D_body_exited(body):
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	print("sign, body is far")
 	if body is Player:
 		player_is_near = false
 		label.visible = false
 		print("sign, player is far")
-
-
-
-func _unhandled_input(event):
-	print("sign, T has been pressed")
-	if event.is_action_pressed("T") and player_is_near:
-		print("sign")
-		label.visible = !label.visible
-	
