d8f9b257d22c6227d673982566c6e56172a7a1b3
dots-rgb <juanvanophem@gmail.com>
2025-08-15 13:13:33 +0200

    slightly better mana_thingy
    -made a small improvement for the mana thingy

--- scripts/UI_and_the_like/mana_thingy.gd
+++ scripts/UI_and_the_like/mana_thingy.gd
@@ -4,6 +4,7 @@ extends Node2D
 @export var my_color: String
 
 @onready var mana_bar = $TextureProgressBar
+@onready var mana_bar2 = $TextureProgressBar2
 
 var all_colors = ["red", "blue", "green", "purple"]
 var used_colors = ["red", "green", "blue", "a", "purple"]
@@ -16,6 +17,7 @@ var mana_recharge_speed : int # damage_mulitiplier = size
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	mana_bar.value = 0
+	mana_bar2.value = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,8 +26,10 @@ func _process(delta: float) -> void:
 	#print("dd ", mana_recharge_multiplier)
 	mana_recharge_speed = mana_recharge_base_speed * mana_recharge_multiplier
 	mana_bar.value += mana_recharge_speed
+	mana_bar2.value += mana_recharge_speed
 	if mana_bar.value == mana_bar.max_value:
 		if filled_orbs < max_orbs:
 			filled_orbs += 1
 			mana_bar.value = 0
+			mana_bar2.value = 0
 	#print("mana_thingy ", my_color, "   ", filled_orbs)
